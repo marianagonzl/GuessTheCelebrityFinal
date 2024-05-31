a639c05d4a200ce689ff44577369540cdae39090
marianagonzl <10193@portalcedi.edu.mx>
2024-05-30 19:36:07 -0600

    documentos lol

---
+++ eleccion.hpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <SFML/Graphics.hpp>
+using namespace std;
+using namespace sf;
+
+class Seleccion
+{
+public:
+    Sprite respuestas;
+    Texture textura;
+};
